200fd6f26727b88a56fe2c3b1c8ef7768a2da14c
Rafael Medeiros Ribeiro <rafaelmedrib@gmail.com>
2022-04-17 10:12:45 -0300

    :tada: initial commit; :sparkles: adds check exists user account

--- src/index.js
+++ src/index.js
@@ -10,7 +10,15 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const userExists = users.find(user => user.username === username);
+
+  if(!userExists) {
+    return response.status(404).json({error: "user not found"});
+  }
+
+  request.user = userExists;
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
