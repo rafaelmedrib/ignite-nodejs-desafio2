67f8266a318cad95d363b8ce01165424f3807dc3
Rafael Medeiros Ribeiro <rafaelmedrib@gmail.com>
2022-04-17 10:53:30 -0300

    :sparkles: adds find user by id

--- src/index.js
+++ src/index.js
@@ -55,7 +55,15 @@ function checksTodoExists(request, response, next) {
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params.id;
+  const user = users.find(user => user.id === id);
+
+  if(!user) {
+    return response.status(404).send();
+  }
+
+  request.user = user;
+  next();
 }
 
 app.post('/users', (request, response) => {
